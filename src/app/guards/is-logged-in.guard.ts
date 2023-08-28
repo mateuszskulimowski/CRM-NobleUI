63b893b3a21a6ed40c119022146cf550ecc50c31
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-08-28 23:55:12 +0200

    is logged in guard

--- src/app/guards/is-logged-in.guard.ts
+++ src/app/guards/is-logged-in.guard.ts
@@ -7,59 +7,24 @@ import {
   UrlTree,
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { catchError, map, switchMap, take } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { UserService } from '../services/user.service';
-import { AuthenticationService } from '../services/authentication.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class IsLoggedInGuard implements CanActivate {
-  constructor(
-    private _userService: UserService,
-    private _router: Router,
-    private _authenticationService: AuthenticationService
-  ) {}
+  constructor(private _userService: UserService, private _router: Router) {}
   canActivate(
-    activatedRoute: ActivatedRouteSnapshot,
+    route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> {
-    // return this._userService.getMe().pipe(
-    //   take(1),
-    //   catchError((error: HttpErrorResponse) => of('error')),
-    //   map((response) => {
-    //     console.log('isLoggedIn');
-    //     console.log(activatedRoute.routeConfig?.path);
-    //     // return this._userService.isVerifiedUser$.pipe(
-    //     //   map((isVerified) => {
-    //     return response === 'error'
-    //       ? this._router.parseUrl(
-    //           activatedRoute.data['redirectLoginUrl'] || '/login'
-    //         )
-    //       : true;
-
-    // return response === 'error' &&
-    //   activatedRoute.routeConfig?.path !== 'login' &&
-    //   activatedRoute.routeConfig?.path !== 'register'
-    //   ? this._router.parseUrl(
-    //       activatedRoute.data['redirectLoginUrl'] || '/login'
-    //     )
-    //   : response !== 'error'
-    //   ? true
-    //   : true;
-    //   })
-    // );
-    // })
-    // );
     return this._userService.getMe().pipe(
       catchError((error: HttpErrorResponse) => of('error')),
       map((response) =>
         response === 'error'
-          ? this._router.parseUrl(
-              activatedRoute.data['redirectLoginUrl'] || '/login'
-            )
+          ? this._router.parseUrl(route.data['redirectLoginUrl'])
           : true
       )
     );
-    return of(true);
   }
 }
