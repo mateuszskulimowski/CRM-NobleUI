7f23c8a1426d3103cc906f794cbd4268854bc320
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-08-29 20:35:35 +0200

    refreshToken interceptor fixed

--- src/app/interceptors/refresh-token.interceptor.ts
+++ src/app/interceptors/refresh-token.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
@@ -8,37 +8,39 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, switchMap, throwError } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
+import { LOCALSTORAGE, LocalStorage } from '../storages/local-storage';
 
 @Injectable()
 export class RefreshTokenInterceptor implements HttpInterceptor {
-  constructor(private _authenticationService: AuthenticationService) {}
+  constructor(
+    private _authenticationService: AuthenticationService,
+    @Inject(LOCALSTORAGE) private _localStorage: LocalStorage
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    const refreshToken = this._localStorage.getItem('refreshToken');
+    const blacListUrl = ['/login', '/register'];
+    if (blacListUrl.find((blackUrl) => request.url.includes(blackUrl))) {
+      return next.handle(request);
+    }
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401 && error.error.message === 'jwt expired') {
-          return this._authenticationService.refreshToken$.pipe(
-            switchMap((refreshToken) => {
-              if (refreshToken) {
-                return this._authenticationService
-                  .refreshToken(refreshToken as string)
-                  .pipe(
-                    switchMap((response) => {
-                      request = request.clone({
-                        setHeaders: {
-                          Authorization: `Bearer ${response.accessToken}`,
-                        },
-                      });
-                      return next.handle(request);
-                    })
-                  );
-              }
-              return next.handle(request);
-            })
-          );
+        if (error.status === 401) {
+          return this._authenticationService
+            .refreshToken(refreshToken as string)
+            .pipe(
+              switchMap((response) => {
+                request = request.clone({
+                  setHeaders: {
+                    Authorization: `Bearer ${response.accessToken}`,
+                  },
+                });
+                return next.handle(request);
+              })
+            );
         }
         return throwError(() => error);
       })
